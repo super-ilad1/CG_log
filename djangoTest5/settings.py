e01e692cc951f44c4c3bcee405b790947cc2412c
ilad <uaremycaptain@gmail.com>
2020-02-15 13:39:04 +0800

    update_setting

--- djangoTest5/settings.py
+++ djangoTest5/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+import socket
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,9 +23,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+q*rc^iuzoa(zfymt(yp^^zp-89=kk(r^8*yr68b0qhcjbf&64'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+if not "-" in  socket.gethostname()[6:]:
+    DEBUG = False
+else:
+    DEBUG=True
+
+
+ALLOWED_HOSTS = [*]
 
 # Application definition
 
@@ -124,6 +130,11 @@ STATIC_URL = '/abc/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+
+if not "-" in  socket.gethostname()[6:]:
+    SATTIC_ROOT=os.path.join(BASE_DIR, "static")
+
 
 CACHES = {
     "default": {
